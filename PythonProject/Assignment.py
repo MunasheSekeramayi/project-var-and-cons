061f591101f71b9710d4e83c68ddd0c25e5464c2
MunasheSekeramayi <r256641h@cuz.ac.zw>
2025-08-19 14:57:08 +0200

    Add files via upload

---
+++ PythonProject/Assignment.py
@@ -0,0 +1,16 @@
+# Constants (by convention, written in uppercase)
+PI = 3.14159
+GRAVITY = 9.8  # m/s²
+
+# Variables
+radius = 5
+height = 10
+
+# Calculations using variables and constants
+volume = PI * radius**2 * height
+weight = GRAVITY * volume
+
+# Output using print()
+print("Cylinder Volume:", volume)
+print("Estimated Weight under Earth's gravity:", weight)
+
